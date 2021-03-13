bc07ed259d06ec864066b0e4ab9ff2f74aff044e
alex <alejandro_n@outlook.com>
2021-03-13 14:07:04 -0600

    Added styled components

---
+++ src/components/FaqSection.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+function FaqSection() {
+    return (
+        <div>
+            <div className="faq">
+                <h2>Any questions <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How Do I start?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Daily Schedule</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Diferrent payment methods</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima.</p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>What product do you offer?</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad, minima.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
